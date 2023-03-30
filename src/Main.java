d4c3756b00173c3a8e374869c54603e40b58e76e
dietrich1208 <62686652+dietrich1208@users.noreply.github.com>
2023-03-30 10:23:30 +0300

    homework6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import hm9.*;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            int i = 0;
+            String path ="src/hm9/Loader.yaml";
+            AbstractLoggerConfigurationLoad abstractLoggerConfigurationLoad = new FileLoggerConfigurationLoader();
+            FileLoggerConfiguration fileLoggerConfiguration = (FileLoggerConfiguration) abstractLoggerConfigurationLoad.load(path);
+            AbstractLogger abstractLogger = new FileLogger(fileLoggerConfiguration);
+            while (i++ < 20){
+                abstractLogger.debug("debug");
+                abstractLogger.info("information");
+
+            }
+        }catch (IOException ex){
+            throw new  RuntimeException(ex);
+        }
+
     }
 }
